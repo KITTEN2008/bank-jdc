0c5bdfe1a9fa62da1aa1df84ed5cfa975fe3257b
KITTEN2008 <elt.sans@bk.ru>
2026-02-11 17:51:29 +0300

    Update constants.js

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,1 +1,19 @@
-
+module.exports = {
+  CURRENCIES: {
+    JDC: { name: 'Жад', rate: 150 },
+    IO: { name: 'Империал', rate: 50 },
+    GHY: { name: 'Расфер', rate: 12 },
+    RUB: { name: 'Рубль', rate: 1 }
+  },
+  
+  CREDIT: {
+    MAX_AMOUNT: 100000,      // Максимальная сумма кредита
+    MIN_AMOUNT: 1000,        // Минимальная сумма
+    BASE_RATE: 10,           // 10% годовых
+    MAX_TERM_DAYS: 365       // Макс срок
+  },
+  
+  JWT: {
+    EXPIRES_IN: '7d'
+  }
+};
