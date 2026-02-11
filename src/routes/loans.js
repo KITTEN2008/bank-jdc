11cfb26f74bb51910166339804dde808a9abc0d8
KITTEN2008 <elt.sans@bk.ru>
2026-02-11 17:56:42 +0300

    Update loans.js

--- src/routes/loans.js
+++ src/routes/loans.js
@@ -1,1 +1,9 @@
+const express = require('express');
+const LoanController = require('../controllers/loanController');
+const auth = require('../middleware/auth');
+const router = express.Router();
 
+router.post('/', auth, LoanController.requestLoan);
+router.get('/', auth, LoanController.getMyLoans);
+
+module.exports = router;
