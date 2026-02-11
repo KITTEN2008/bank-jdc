a5b0ab17903de99f7a91f28ebbfc077e07469c8e
KITTEN2008 <elt.sans@bk.ru>
2026-02-11 17:56:05 +0300

    Update auth.js

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,1 +1,10 @@
+const express = require('express');
+const AuthController = require('../controllers/authController');
+const auth = require('../middleware/auth');
+const router = express.Router();
 
+router.post('/register', AuthController.register);
+router.post('/login', AuthController.login);
+router.get('/me', auth, AuthController.getMe);
+
+module.exports = router;
