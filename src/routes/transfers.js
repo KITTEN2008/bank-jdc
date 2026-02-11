44ba6780580b0a7951526a5bc0dc76452f7bc741
KITTEN2008 <elt.sans@bk.ru>
2026-02-11 17:56:26 +0300

    Update transfers.js

--- src/routes/transfers.js
+++ src/routes/transfers.js
@@ -1,1 +1,9 @@
+const express = require('express');
+const TransferController = require('../controllers/transferController');
+const auth = require('../middleware/auth');
+const router = express.Router();
 
+router.post('/', auth, TransferController.transfer);
+router.get('/history', auth, TransferController.getHistory);
+
+module.exports = router;
