ae9cdadf9227cc26718c57b49f18bd3d064d8168
KITTEN2008 <elt.sans@bk.ru>
2026-02-11 17:56:58 +0300

    Update invoices.js

--- src/routes/invoices.js
+++ src/routes/invoices.js
@@ -1,1 +1,11 @@
 
+const express = require('express');
+const InvoiceController = require('../controllers/invoiceController');
+const auth = require('../middleware/auth');
+const router = express.Router();
+
+router.post('/', auth, InvoiceController.createInvoice);
+router.get('/pending', auth, InvoiceController.getPendingInvoices);
+router.post('/:id/pay', auth, InvoiceController.payInvoice);
+
+module.exports = router;
